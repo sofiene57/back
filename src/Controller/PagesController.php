ed62ae826081364e9222290646fde6673882cd65
sofiene <sofiane-g@hotmail.fr>
2022-04-06 11:55:02 +0200

    mise en place du css sur les pages et crud

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PagesController extends AbstractController
+{
+    #[Route('/', name: 'accueil')]
+    public function index(): Response
+    {
+        return $this->render('pages/accueil.html.twig', [
+            'controller_name' => 'PagesController',
+        ]);
+    }
+
+    #[Route('/service', name: 'service')]
+    public function service(): Response
+    {
+        return $this->render('pages/service.html.twig', [
+            'controller_name' => 'PagesController',
+        ]);
+    }
+
+    #[Route('/coach', name: 'coach')]
+    public function coach(): Response
+    {
+        return $this->render('pages/coach.html.twig', [
+            'controller_name' => 'PagesController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('pages/contact.html.twig', [
+            'controller_name' => 'PagesController',
+        ]);
+    }
+}
