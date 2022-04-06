41f0e5af59ab094a8bae18eaa91f7720df4cdcff
sofiene <sofiane-g@hotmail.fr>
2022-04-06 13:53:27 +0200

    creation d'un admin mdp, acces

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'login')]
+    public function index(AuthenticationUtils $AuthenticationUtils): Response
+    {if($this->getUser()){
+        return $this->redirectToRoute('accueil');
+    }
+        // get the login error if there is one
+        $error = $AuthenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $AuthenticationUtils->getLastUsername();
+        {
+            return $this->render('login/index.html.twig', [
+                'controller_name' => 'LoginController',
+                'last_username' => $lastUsername,
+            'error' => $error,
+            ]);
+        }
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): void
+    {
+        
+    }
+}
